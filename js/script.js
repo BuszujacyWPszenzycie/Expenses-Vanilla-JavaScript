ac687a749cc805c245b9bc7ebb2fcb4dbdf1c39c
Marcin <marcin.szturminski@gmail.com>
2024-01-26 10:36:46 +0100

    corrected delete option

--- js/script.js
+++ js/script.js
@@ -6,59 +6,36 @@ const errorDialog = document.querySelector('dialog')
 const allInputs = document.querySelectorAll('.input')
 const sidebar = document.querySelector('.sidebar')
 const showExpenseModal = document.querySelector('.show')
-const expenseItemforTesting = document.querySelector('.expense__item')
 const showTitle = document.querySelector('.show__title')
 const showClient = document.querySelector('.show__client')
 const showDate = document.querySelector('.show__date')
 const showDescritpion = document.querySelector('.show__description')
 const showAmount = document.querySelector('.show__amount')
+const deleteExpenseButton = document.querySelector('.show__btn--delete')
 
-const EXPENSES = [
-	// {
-	// 	invoiceNumber: 'FA/12/2024',
-	// 	client: 'ASEA',
-	// 	invoiceDate: '2024-12-01',
-	// 	description: 'Coś tam',
-	// 	amount: 125,
-	// },
-	// {
-	// 	invoiceNumber: 'FA/13/2024',
-	// 	client: 'ASEA',
-	// 	invoiceDate: '2024-12-01',
-	// 	description: 'Coś tam',
-	// 	amount: 125,
-	// },
-]
-
-// expenseItemforTesting.addEventListener('click', showExpense)
+const EXPENSES = []
 
 function showExpense(e) {
-	const index = EXPENSES.findIndex(x => x.invoiceNumber === e.target.textContent)
+	const index = EXPENSES.findIndex(index => index.invoiceNumber === e.target.textContent)
 	showTitle.textContent = EXPENSES[index].invoiceNumber
 	showClient.textContent = EXPENSES[index].client
 	showDate.textContent = EXPENSES[index].invoiceDate
 	showDescritpion.textContent = EXPENSES[index].description
 	showAmount.textContent = EXPENSES[index].amount
 	showExpenseModal.classList.add('show-expense')
+}
 
-	const deleteBtn = document.querySelector('.show__btn--delete')
-	deleteBtn.addEventListener('click', () => {
-		const allExpenseItems = document.querySelectorAll('.expense__item')
-		const allExpenseItemsConverted = [...allExpenseItems]
-		allExpenseItems[index].remove()
-
-		console.log(allExpenseItems)
-		console.log(EXPENSES)
-		console.log('----------------------')
-		EXPENSES.splice(index, 1)
-		console.log(allExpenseItems)
-		console.log(EXPENSES)
-		showExpenseModal.classList.remove('show-expense')
-	})
+function deleteExpenseFunction() {
+	const activeExpense = document.querySelector('.show__title')
+	const index = EXPENSES.findIndex(index => index.invoiceNumber === activeExpense.textContent)
+	const allExpenseItems = document.querySelectorAll('.expense__item')
+	allExpenseItems[index].remove()
+	EXPENSES.splice(index, 1)
+	showExpenseModal.classList.remove('show-expense')
 }
 
 function saveFunction() {
-	if (checkAllInputs()) {
+	if (checkAllInputsFunction()) {
 		errorDialogFunction()
 	} else {
 		closeModalFunction()
@@ -76,14 +53,13 @@ function saveFunction() {
 		sidebar.appendChild(expenseItem)
 		console.log(EXPENSES)
 		expenseItem.addEventListener('click', showExpense)
+		allInputs.forEach(input => {
+			input.value = ''
+		})
 	}
-
-	allInputs.forEach(input => {
-		input.value = ''
-	})
 }
 
-function checkAllInputs() {
+function checkAllInputsFunction() {
 	for (let i = 0; i < allInputs.length; i++) {
 		if (allInputs[i].value.trim() === '') {
 			return true
@@ -109,3 +85,48 @@ function errorDialogFunction() {
 
 cancelBtn.addEventListener('click', closeModalFunction)
 saveBtn.addEventListener('click', saveFunction)
+deleteExpenseButton.addEventListener('click', deleteExpenseFunction)
+
+// NOTES
+
+// EXAMPLES	OF INVOCIES IF NEEDED
+
+// {
+// 	invoiceNumber: 'FA/12/2024',
+// 	client: 'ASEA',
+// 	invoiceDate: '2024-12-01',
+// 	description: 'Coś tam',
+// 	amount: 125,
+// },
+// {
+// 	invoiceNumber: 'FA/13/2024',
+// 	client: 'ASEA',
+// 	invoiceDate: '2024-12-01',
+// 	description: 'Coś tam',
+// 	amount: 125,
+// },
+// {
+// 	invoiceNumber: 'FA/15/2024',
+// 	client: 'ASEA',
+// 	invoiceDate: '2024-12-01',
+// 	description: 'Coś tam',
+// 	amount: 125,
+// },
+
+// FUNCION TO BE ABLE TO RUN MANUALY ADDED INVOICES
+
+// expenseItemforTesting.forEach(item => {
+// 	item.addEventListener('click', showExpense)
+// })
+
+// expenseItemforTesting.addEventListener('click', showExpense)
+
+// BUTTONS OF EXPENSE ITEMS
+
+// <button class="expense__item">FA/12/2024</button>
+// <button class="expense__item">FA/13/2024</button>
+// <button class="expense__item">FA/15/2024</button>
+
+// EXPENSE ITEMS
+
+// const expenseItemforTesting = document.querySelectorAll('.expense__item')
